805291c82d5e2fe954c8fd90f6809c338aba63f1
Carlos Toro <csantostv@outlook.com>
2022-12-14 23:07:48 -0500

    day 6 solution

--- src/main/java/csantve/adventofcode/solutions/Day6.java
+++ src/main/java/csantve/adventofcode/solutions/Day6.java
@@ -12,7 +12,29 @@ public class Day6 extends Day{
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
             String line = br.readLine();
-            System.out.println(line);
+            for (int i = 0; i < line.length() - 4; i++) {
+                String sub = line.substring(i, i + 4);
+                if (hasUniqueCharacters(sub)) {
+                    System.out.println(i + 4);
+                    break;
+                }
+            }
+            for (int i = 0; i < line.length() - 14; i++) {
+                String sub = line.substring(i, i + 14);
+                if (hasUniqueCharacters(sub)) {
+                    System.out.println(i + 14);
+                    break;
+                }
+            }
         } catch (IOException ignore) {}
+    }
+
+    private boolean hasUniqueCharacters(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j < string.length(); j++) {
+                if (string.charAt(i) == string.charAt(j)) return false;
+            }
+        }
+        return true;
     }
 }
