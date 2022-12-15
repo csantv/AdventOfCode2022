48d34fc84b5fdadcb248a3afd78511cb5a06b558
Carlos Toro <csantostv@outlook.com>
2022-12-14 21:18:29 -0500

    day 3 and day 4 solutions

--- src/main/java/csantve/adventofcode/Main.java
+++ src/main/java/csantve/adventofcode/Main.java
@@ -3,6 +3,8 @@ package csantve.adventofcode;
 public class Main {
     public static void main(String[] args) {
         Solutions solutions = new Solutions();
+        solutions.day4();
+        solutions.day3();
         solutions.day2();
         solutions.day1();
     }
--- src/main/java/csantve/adventofcode/Solutions.java
+++ src/main/java/csantve/adventofcode/Solutions.java
@@ -8,9 +8,77 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solutions {
     private static final ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+    public void day4() {
+        System.out.println("=== DAY 4 SOLUTION ===");
+
+        try (InputStream is = getResource("day4.txt")) {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            long repeats = 0;
+            long repeats2 = 0;
+            while ((line = br.readLine()) != null) {
+                var groups = line.split(",");
+                var g1 = IntStream.rangeClosed(Integer.parseInt(groups[0].split("-")[0]),
+                        Integer.parseInt(groups[0].split("-")[1])).boxed().collect(Collectors.toSet());
+                var g2 = IntStream.rangeClosed(Integer.parseInt(groups[1].split("-")[0]),
+                        Integer.parseInt(groups[1].split("-")[1])).boxed().collect(Collectors.toSet());
+                if (g1.containsAll(g2) || g2.containsAll(g1)) repeats++;
+                if (!Collections.disjoint(g1, g2)) repeats2++;
+            }
+            System.out.println(repeats);
+            System.out.println(repeats2);
+        } catch (IOException ignore) {}
+    }
+
+    public void day3() {
+        System.out.println("=== DAY 3 SOLUTION ===");
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String priorities = alphabet + alphabet.toUpperCase();
+
+        try (InputStream is = getResource("day3.txt")) {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            long prio = 0;
+            while ((line = br.readLine()) != null) {
+                String first = line.substring(0, line.length() / 2);
+                String second = line.substring(line.length() / 2);
+                for (String ch: first.split("")) {
+                    if (second.contains(ch)) {
+                        prio += priorities.indexOf(ch) + 1;
+                        break;
+                    }
+                }
+            }
+            System.out.println(prio);
+        } catch (IOException ignore) {}
+
+        try (InputStream is = getResource("day3.txt")) {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line1;
+            long prio = 0;
+            while ((line1 = br.readLine()) != null) {
+                String line2 = br.readLine();
+                String line3 = br.readLine();
+                for (String ch: line1.split("")) {
+                    if (line2.contains(ch) && line3.contains(ch)) {
+                        prio += priorities.indexOf(ch) + 1;
+                        break;
+                    }
+                }
+            }
+            System.out.println(prio);
+        } catch (IOException ignore) {}
+    }
 
     public void day2() {
         // X, A -> rock
